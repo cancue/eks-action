b7558572215d3ca68303b9d2541375a554043a98
cancue <kanqmh@gmail.com>
2020-03-18 18:26:54 +0900

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
+#!/bin/sh -l
+
+set -xe
+
 sh -c "aws configure set region ${aws_region}"
 sh -c "aws configure set aws_access_key_id ${aws_access_key_id}"
 sh -c "aws configure set aws_secret_access_key ${aws_secret_access_key}"
